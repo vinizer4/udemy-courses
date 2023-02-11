46eca11f167fc8f9ab967210abf84174c06eac6f
Vinicius Teixeira Saraiva <vinicius.ts.online@gmail.com>
2023-02-11 12:35:42 -0300

    Fix

--- SpringBootExpertDouglas Souza/curso-spring-boot-especialista-master/pedidos-api/src/main/java/io/github/dougllasfps/exception/RegraNegocioException.java
+++
@@ -1,8 +0,0 @@
-package io.github.dougllasfps.exception;
-
-public class RegraNegocioException extends RuntimeException {
-
-    public RegraNegocioException(String message) {
-        super(message);
-    }
-}
